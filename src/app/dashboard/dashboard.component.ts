0a7a15f56aad086abd20f2cb79ff0a6d111c0a65
Sudheesh <ohyezsudhi@gmail.com>
2022-05-20 19:18:47 +0530

    Minor changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { decimalDigest } from '@angular/compiler/src/i18n/digest';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EventsComponent } from '../events/events.component';
 import { DataService } from '../services/data.service';
@@ -13,33 +14,37 @@ import { DataService } from '../services/data.service';
 })
 export class DashboardComponent implements OnInit {
 
-  myDate=""
-  description=""
-  currentUser=JSON.parse(localStorage.getItem('currentUser') || '')
-  currentId=""
-  updateId=0
+  myDate = ""
+  description = ""
+  currentUser = JSON.parse(localStorage.getItem('currentUser') || '')
+  currentId = ""
+  updateId = 0
 
+  addForm = new FormGroup({
+    myDate: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required)
+  })
 
-  constructor(private ds:DataService,private router:Router) {
-   }
+  constructor(private ds: DataService, private router: Router,) {
+  }
 
   ngOnInit(): void {
     this.ds.msg$
-    .subscribe(message=>{
-      if(message){
-        this.updateId=1
-      }
-    })
+      .subscribe(message => {
+        if (message) {
+          this.updateId = 1
+        }
+      })
   }
-  
+
   deleteFromParent() {
-    this.currentId = JSON.parse(localStorage.getItem("currentId")||'')
+    this.currentId = JSON.parse(localStorage.getItem("currentId") || '')
   }
   onCancel() {
     this.currentId = ""
   }
   onDelete() {
-    this.currentId = JSON.parse(localStorage.getItem("currentId")||'')
+    this.currentId = JSON.parse(localStorage.getItem("currentId") || '')
 
     this.ds.onDelete(this.currentId)
       .subscribe((result: any) => {
@@ -53,21 +58,29 @@ export class DashboardComponent implements OnInit {
         }
       )
   }
-  addEvent(){
-    let eventDate=this.myDate
-    let eventDescription=this.description
-    const userId= JSON.parse(localStorage.getItem('currentId') || '')
-    this.ds.addEvent(userId,eventDate, eventDescription)
+  addEvent() {
+    let eventDate = this.addForm.value.myDate
+    let eventDescription = this.addForm.value.description
+    const userId = JSON.parse(localStorage.getItem('currentId') || '')
+    if (this.addForm.valid) {
+      this.ds.addEvent(userId, eventDate, eventDescription)
         .subscribe((result: any) => {
           if (result) {
             alert(result.message)
             location.reload();
           }
         },
-          (result:any) => {
+          (result: any) => {
             alert(result.error.message)
           }
         )
+    }
+    else{
+      alert('Please fill all the fields')
+    }
+  }
+  get addDate(){
+    return this.addForm.get('myDate')
   }
   logout() {
     localStorage.removeItem("currentUser")
--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { EventsComponent } from '../events/events.component';
 import { DataService } from '../services/data.service';
-import { FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -12,49 +12,58 @@ import { FormControl } from '@angular/forms';
 export class UpdateComponent implements OnInit {
   newDate = ""
   newDescription = ""
-  updateId=0
+  updateId = 0
   oldDate = ''
-  oldDescription=''
-  data:any=[]
+  oldDescription = ''
+  data: any = []
+
+  updateForm = new FormGroup({
+    newDate: new FormControl('', Validators.required),
+    newDescription: new FormControl('', Validators.required)
+  })
 
 
-  constructor(private ds: DataService,private fb:FormBuilder) {
+  constructor(private ds: DataService, private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.ds.msg$
-    .subscribe(message=>{
-      if(message){
-        this.updateId=1
-        this.oldDate=message[0]
-        this.oldDescription=message[1]
-      }
-    })
+      .subscribe(message => {
+        if (message) {
+          this.updateId = 1
+          this.oldDate = message[0]
+          this.oldDescription = message[1]
+        }
+      })
   }
   updateEvent() {
-    let oldDate=this.oldDate
-    let oldDescription=this.oldDescription
-    let newDate=this.newDate
-    let newDescription=this.newDescription
+    let oldDate = this.oldDate
+    let oldDescription = this.oldDescription
+    let newDate = this.updateForm.value.newDate
+    let newDescription = this.updateForm.value.newDescription
     this.data.push(oldDate)
     this.data.push(oldDescription)
     this.data.push(newDate)
     this.data.push(newDescription)
-    const userId= JSON.parse(localStorage.getItem('currentId') || '')
-    this.ds.editEvent(userId,this.data)
+    const userId = JSON.parse(localStorage.getItem('currentId') || '')
+    if (this.updateForm.valid) {
+      this.ds.editEvent(userId, this.data)
         .subscribe((result: any) => {
           if (result) {
             alert(result.message)
             location.reload();
           }
         },
-          (result:any) => {
+          (result: any) => {
             alert(result.error.message)
           }
         )
-  
+    }
+    else{
+      alert('Please fill all the fields')
+    }
   }
-  cancelUpdate(){
+  cancelUpdate() {
     location.reload()
   }
 
